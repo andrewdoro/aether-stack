a18d87a770d5000ad659f894bc7281dae6016341
andrewdoro <elfandreis@gmail.com>
2023-04-18 23:44:46 +0300

    fix: ssr solid localStorage

--- apps/web/src/components/utils.tsx
+++ apps/web/src/components/utils.tsx
@@ -5,9 +5,9 @@ export function createLocalStore<T extends object>(
   name: string,
   init: T
 ): [Store<T>, SetStoreFunction<T>] {
-  const localState = localStorage.getItem(name);
+  const localState = localStorage?.getItem(name);
   const [state, setState] = createStore<T>(localState ? JSON.parse(localState) : init);
 
-  createEffect(() => localStorage.setItem(name, JSON.stringify(state)));
+  createEffect(() => localStorage?.setItem(name, JSON.stringify(state)));
   return [state, setState];
 }
